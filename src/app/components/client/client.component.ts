1946d83ab1a2c8e3e33f639d561f8c33e4ed1993
visit2shobhit <shobhit833@gmail.com>
2026-01-14 11:20:06 -0800

    List Client Data

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Client } from '../../model/class/Client';
+import { FormsModule } from '@angular/forms';
+import { ClientService } from '../../services/client.service';
 
 @Component({
   selector: 'app-client',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
-export class ClientComponent {
-
+export class ClientComponent implements OnInit {
+   clientObject:Client = new Client();
+   clientList:Client[] = [];
+   clientService=inject(ClientService);
+   ngOnInit(): void {
+     this.loadAllClients();
+   }
+   loadAllClients(){
+      this.clientService.getAllClients().subscribe((res:any)=>{
+        console.log('testestest');
+        
+        console.log(res.data,'RESRES');
+        
+        this.clientList = res.data;
+      });
+   }
+   saveClient(){
+      this.clientService.addUpdate(this.clientObject).subscribe((res:any)=>{
+        if(res.result){
+          alert('Client Saved Successfully');
+          this.loadAllClients();
+        } else{
+          alert('Error while saving client');
+        }
+      });
+   }
 }
---
+++ src/app/services/client.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Client } from '../model/class/Client';
+import { environment } from '../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+  constructor(private http: HttpClient) {
+  }
+  getAllClients(){
+    return this.http.get(environment.API_URL + "GetAllClients");
+  }
+  addUpdate(obj:Client){
+    return this.http.post(environment.API_URL + "GetAllClients", obj);
+  }
+  deleteClientById(id:number){
+    return this.http.delete(environment.API_URL + "DeleteClientByClientId?clientId="+id);
+  }
+}
